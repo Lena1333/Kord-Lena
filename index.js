91b252e1231f7718401a9fbb4fd6982efac6a6e1
Lena1333 <126816886+Lena1333@users.noreply.github.com>
2023-03-02 21:10:41 +0200

    JS 11 Lena HOMEWORK

---
+++ index.js
@@ -0,0 +1,20 @@
+// ДЗ: Налаштування, змiннi
+
+let firstParametr = prompt("Enter your number:");
+let secondParametr = prompt("Enter your second number:");
+
+
+let result = Number(firstParametr) + Number(secondParametr);
+console.log("Sum:", result);
+
+
+let resultSecond = firstParametr - secondParametr;
+console.log(resultSecond);
+
+let resultThird = firstParametr * secondParametr;
+console.log(resultThird);
+
+let resultFourth = firstParametr / secondParametr;
+console.log(resultFourth)
+
+
